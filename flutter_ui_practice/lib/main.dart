4f255968c3d8a1a99e155522623ab6d2a9e4fa51
Dhanu Sunar <35205798+dhanu-sunar@users.noreply.github.com>
2022-09-16 15:16:48 +0930

    master

--- flutter_ui_practice/lib/main.dart
+++ flutter_ui_practice/lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() =>  runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('An App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'Yo',
-              style: TextStyle(
-          fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'
-      ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('CCLLIICCKK!!'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('An App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Yo',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('CCLLIICCKK!!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
